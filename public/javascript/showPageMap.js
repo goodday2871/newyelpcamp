6bd827bed261d28beb9969594da6d1f22e810e17
goodday2871 <goodday2871@gmail.com>
2020-11-06 14:30:31 +0800

    Revert "Revert "var change""
    This reverts commit 449c03735c2ec3ed9c5b6a1eca0e851290b6d0a9.

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,6 +1,6 @@
 
 mapboxgl.accessToken = mapToken
-const map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11', 
 center: campground.geometry.coordinates,
